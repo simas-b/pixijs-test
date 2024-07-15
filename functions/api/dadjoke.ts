58b37160042761e4be8f50c2c56493b4e1df0474
Simas Butavičius <simasbutavicius@gmail.com>
2024-07-15 18:50:42 +0300

    Remove intermediate variable

--- functions/api/dadjoke.ts
+++ functions/api/dadjoke.ts
@@ -1,10 +1,9 @@
 export const onRequestGet: PagesFunction = async () => {
-  const result = await fetch("https://icanhazdadjoke.com/", {
+  const joke = await fetch("https://icanhazdadjoke.com/", {
     headers: {
       accept: "application/json",
     },
   });
-  const joke = await result.json();
 
-  return Response.json(joke);
+  return Response.json(await joke.json());
 };
